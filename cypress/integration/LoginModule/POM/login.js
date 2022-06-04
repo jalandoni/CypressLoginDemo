ed87b384b05f8888e70859e6203cdddeda894074
chloe868 <herreracherrymae@gmail.com>
2022-06-04 14:31:51 +0800

    For passing

--- cypress/integration/LoginModule/POM/login.js
+++ cypress/integration/LoginModule/POM/login.js
@@ -1,14 +1,35 @@
 ///<reference types="cypress" />
-import user from '../fixtures/user.json'
-const user = new user()
 
 class Login{
-    init(){
-        cy.fixture('user').then((user) => {
-            this.user = user
-            cy.get('').type(this.user.username)
-            cy.get('').type(this.user.password)
-          })
+
+    submit(){
+        cy.fixture('user').then(function (testdata) {
+            this.user = testdata
+            //click submit button with incorrect username and password
+            cy.get('#uid').type('user')
+            cy.get('[name="password"]').type('pass123')
+            cy.get('[type="submit"]').click()
+             cy.wait(2000)
+            cy.get('[name="password"]').should('have.value','')
+            cy.get('#uid').clear()
+            cy.get('[name="password"]').clear()
+            
+            
+
+            //click submit button with incorrect username and correct password
+            cy.get('#uid').type('user')
+            cy.get('[name="password"]').type(this.user.password)
+            cy.get('[type="submit"]').click()
+            cy.get('[name="password"]').should('have.value','')
+            cy.get('#uid').clear()
+            cy.get('[name="password"]').clear()
+            
+            
+            //correct password and username
+            cy.get('#uid').type(this.user.username).should('have.value',this.user.username)
+            cy.get('[name="password"]').type(this.user.password).should('have.value',this.user.password)
+            cy.get('[type="submit"]').click()
+        })
     }
 
 }export default Login
--- cypress/integration/LoginModule/loginpage.spec.js
+++ cypress/integration/LoginModule/loginpage.spec.js
@@ -1,12 +1,24 @@
 /// <reference types="cypress" />
 import Login from "./POM/login"
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
 
-describe('validate login',() =>{
+describe('Validate login',() =>{
 
     const login = new Login()
+    before('Validate url',()=> {
+        cy.visit('http://35.77.45.137:8080/exam/login') 
+    })
     
-    it('',()=>{
-        cy.visit()
+    it('Validate credentials',()=>{
+        login.submit() 
+    })
+
+    after('Validate logout and visit other page',()=>{
+        cy.wait(3000)
+        cy.get('[href="login"]').click()
+        
         
     })
 
